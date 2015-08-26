86c7dead157a0cc1853cbefdc7a0af5f1ac33131
Fendirain <fendirainmod@gmail.com>
2015-08-25 20:35:28 -0700

    Update version & quick fix.

--- src/main/java/fendirain/fendirain/init/ModRecipes.java
+++ src/main/java/fendirain/fendirain/init/ModRecipes.java
@@ -7,7 +7,7 @@ public class ModRecipes {
 
     public static void init() {
         // Add new shaped ore recipe.
-        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockFendi), "fff", "f nf", "fff", 'f', ModItems.fendiPiece);
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockFendi), "fff", "f f", "fff", 'f', ModItems.fendiPiece);
         //GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.mapleLeaf), " s ", "sss", " s ", 's', "stickWood"));
 
         //GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.redstoneInfusedFendi), ModItems.fendi, Items.redstone, Items.redstone, Items.redstone, Items.redstone));
